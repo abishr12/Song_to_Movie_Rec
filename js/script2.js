297e4997bb64f34ed23c87e764288161b108a096
Michelle Hamdani <michellehamdani@gmail.com>
2017-09-26 23:37:17 -0700

    added mood buttons and themovieb.org API

--- js/script2.js
+++ js/script2.js
@@ -15,56 +15,6 @@ movieApp.getMovies = function(year){
 			language: "en-US",
 			include_adult: false,
 			certification: 'R',
-			total_results: 9
-		}
-	}).then(function(res){
-		var moviesOutput = res.results;
-		console.log(moviesOutput);
-		// this only sends the DATA of the moviesOutput variable to displayMovies we use an arbitrary var 'item' in this case to add a new name to the data so we can loop over it using the forEach method
-		movieApp.displayMovies(moviesOutput);
-
-
-	})
-}
-
-movieApp.browserStuff = function(){
-	$('.fader').addClass('animated fadeInRight');
-	$('i').addClass('animated infinite bounce');
-	$('a').smoothScroll({
-			offset:0,
-			speed: 400
-		});
-
-
-
-	$(window).scroll(function() {
-    if ($(this).scrollTop() > 0) {
-       $('.prelude h3').addClass('animated fadeInLeft');
-    }
-
-
-
-
-});
-}
-
-var movieApp = {};
-
-movieApp.apiKey = 'ca6ea09713defa345edd21995ca6f8f8';
-
-movieApp.getMovies = function(year){
-
-	$.ajax({
-		url: `https://api.themoviedb.org/3/discover/movie?`,
-		method: 'GET',
-		dataType: 'jsonp',
-		data: {
-			api_key: movieApp.apiKey,
-			primary_release_year: year,
-			certification_country: 'US',
-			language: "en-US",
-			include_adult: false,
-			certification: 'R',
 			page: 1
 		}
 	}).then(function(res){
@@ -118,9 +68,43 @@ movieApp.displayMovies = function(item){
 }
 
 
+movieApp.event = function(){
+	$('form').on('submit', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+	        scrollTop: $('#dynamicInputs').offset().top
+	      }, 1000);
+		var ageInput = Number($('select').val());
+		var birthYear = Number($('input').val());
+		// console.log(ageInput);
+		var userInput = (birthYear + ageInput);
+		if (birthYear > 2000){
+			swal({
+			  title: "You're too young!",
+			  imageUrl: "assets/fritzthecat.jpg",
+			  imageSize: "400x400"
+			});
+		}
+		else if (birthYear <= 1965) {
+			swal({
+				type: "info",
+				title: `The MPAA R rating was not in effect when you were ${ageInput}`
+
+
+			})
+		}
+		else {
+			movieApp.getMovies(userInput);
+		}
+	});
+	// on change event listener on the age om change will first check to see if there are 4 digits in the input and if there are then it makes the request and goes and gets the data.
+}
+
+
 
 movieApp.init = function(){
 	movieApp.getMovies();
+	movieApp.event();
 	movieApp.browserStuff();
 
 }
